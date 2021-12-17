28bbe38d8d6efa6c4855ca8c78786d43b3af920d
Eric C <erikcaul@gmail.com>
2021-12-16 20:53:16 -0500

    add Class game.py and round.py

---
+++ game.py
@@ -0,0 +1,10 @@
+class game:
+    """Patern for a game"""
+    def __init__(self, player_1, player_2, player_1_win, player_2_win):
+        """Initialize a game"""
+        self.player_1 = player_1
+        self.player_2 = player_2
+        self.player_1_win = player_1_win
+        self.player_2_win = player_2_win
+
+
