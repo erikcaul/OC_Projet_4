d7a352c53f4b15eaf1f7f3e5aefd42b6718170e1
erikcaul <erikcaul@gmail.com>
2021-12-17 08:06:47 -0500

    suppression of self.tour in Tournament

--- tournament.py
+++ tournament.py
@@ -1,11 +1,10 @@
 class Tournament:
     """Patern for tournaments"""
-    def __init__(self, name, place, date, tour, rounds, players, time_controller, tunrs_number=4, description=""):
+    def __init__(self, name, place, date, rounds, players, time_controller, tunrs_number=4, description=""):
         """initialize tournament"""
         self.name = name
         self.place = place
         self.date = date
-        self.tour = tour
         self.rounds = rounds
         self.players = players
         self.time_controller = time_controller
