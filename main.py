bc6b1a5479db4cd15855d02d7af41cc765b7dcf1
erikcaul <erikcaul@gmail.com>
2022-01-10 10:17:43 -0500

    test sur main.py en mode main_loop

---
+++ main.py
@@ -0,0 +1,9 @@
+from controlers import menu_controller
+from views import menu_view
+
+"""main loop"""
+
+active = True
+while active:
+    main_loop = menu_controller.MainLoop(menu_view=menu_view.MenuView)
+    menu_controller.MainLoop.run(main_loop)
