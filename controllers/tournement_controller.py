bddaca72733a94f8280a28cdbab0160695c9f184
erikcaul <erikcaul@gmail.com>
2022-01-10 22:51:37 -0500

    update tournament_controller.py

--- controllers/tournement_controller.py
+++ controllers/tournement_controller.py
@@ -1,16 +1,21 @@
 """Tournaments Controller"""
+from views.tournament_view import TournamentView
+from models.tournament import Tournament
 
 
 class TournamentController:
 
-    def __init__(self, view_tournament_creation):
-        self.view_tournament_creation = view_tournament_creation
+    def __init__(self):
+        self.tournament_view = TournamentView()
 
     def new_tournament(self):
-        self.view_tournament_creation.prompt_tournament_info()
+        new_tournement_info = self.tournament_view.prompt_tournament_creation()
+        rounds = []
+        players = []
+        new_tournament = Tournament(new_tournement_info[0], new_tournement_info[1], new_tournement_info[2], rounds, players, new_tournement_info[4], new_tournement_info[3], new_tournement_info[5])
+        return new_tournament
 
     def add_player(self):
         # appeler la vue "tournament" avec différentes fonctionalités, dont pour ajouter player
-        pass
-
+        self.tournament_view.prompt_add_player()
 
