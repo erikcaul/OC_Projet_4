58e98aad7904ed568964387ea1b72f119d4f8b69
erikcaul <erikcaul@gmail.com>
2022-01-10 22:52:13 -0500

    update tournament.py

--- models/tournament.py
+++ models/tournament.py
@@ -1,14 +1,12 @@
 class Tournament:
     """Patern for tournaments"""
-    def __init__(self, name, place, date, rounds, players, time_controller, tunrs_number=4, description=""):
+    def __init__(self, name, location, date, rounds, players, time_controller, tunrs_number=4, description=""):
         """initialize tournament"""
         self.name = name
-        self.place = place
+        self.location = location
         self.date = date
         self.rounds = rounds
         self.players = players
         self.time_controller = time_controller
         self.turns_number = tunrs_number
         self.description = description
-
-   
