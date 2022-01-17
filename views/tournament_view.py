689ad41dc2e2cfa57ced81d0e2fa2437f983bc8e
erikcaul <erikcaul@gmail.com>
2022-01-17 12:48:19 -0500

    modification session

--- views/tournament_view.py
+++ views/tournament_view.py
@@ -7,6 +7,9 @@ class TournamentView:
 
     def prompt_tournament_creation(self):
         """prompt info for the creation of a tournament"""
+        # faire un dict plutôt qu'une liste avec initialisation à vide
+        # valider ce qui rentre (ne pas accepter qu'il ne rentre irne du tout et n'accpeter que les 3 types prévus, et nombres de tours, dates, etc. avec un retry)
+        # fonction d'input avec validation
         tournament_info = ['name', 'location', 'date', 0, 'bullet', 'description']
         tournament_info[0] = input('Enter the tournament name : ')
         tournament_info[1] = input('Enter the location of the tournament : ')
@@ -19,6 +22,8 @@ class TournamentView:
 
     def prompt_add_player(self):
         """prompt for adding player to a tournament"""
+        # récupérer la lste des players et la liste des tournaments
+        # afficher liste tournois 1 ... n et demande de choisir  quel tourois, pareil pour players qui sont pas encore dans tournoi
         tournament_name = input('Please indicate the name of the tournament for which you want to add a player : ')
         player_name = input("Please indicate the player's name you want to add to the tournament : ")
         
