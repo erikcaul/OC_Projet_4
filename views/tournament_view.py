a5c478c6b9c934ebf26454c9d6bc9b055f086de3
erikcaul <erikcaul@gmail.com>
2022-01-24 12:21:26 -0500

    test tournament view validation entry

--- views/tournament_view.py
+++ views/tournament_view.py
@@ -1,5 +1,6 @@
 """Tournament view"""
 from multiprocessing.sharedctypes import Value
+from unicodedata import digit
 from models.tournament import Tournament
 
 class TournamentView:
@@ -10,30 +11,37 @@ class TournamentView:
         """prompt info for the creation of a tournament"""
         # faire un dict plutôt qu'une liste avec initialisation à vide
         tournament_dict = {
-            "name": input('Enter the tournament name : '),
-            "location": input('Enter the location of the tournament : '),
-            "date": input('Enter the date of the tournament : '),
-            "turns_number": input('Enter the number of turns for the tournament : '),
-            "time_controller": input('Please select the type for the time controlling (1. bullet, 2. blitz, 3. quick hit) : '),
-            "description": input('Please enter a description for the tournament: ')
+            "name": 'Enter the tournament name : ',
+            "location": 'Enter the location of the tournament : ',
+            "date": 'Enter the date of the tournament : ',
+            "turns_number": 'Enter the number of turns for the tournament : ',
+            "time_controller": 'Please select the type for the time controlling (1. bullet, 2. blitz, 3. quick hit) : ',
+            "description": 'Please enter a description for the tournament: '
         }
-
-        for key in tournament_dict:
-            value = tournament_dict[key]
-            for value in tournament_dict:
-                if value in tournament_dict:
-                    if value != "":
-                        tournament_dict[key] = value
-                else: 
-                    tournament_dict()
-                
+        tournament_info = {
+            "name":'',
+            "location":'',
+            "date":'',
+            "turns_number":'',
+            "time_controller":'',
+            "description":''}
+        for key, value in tournament_info.items():
+            while (value == "" or value == None):
+                value = input(tournament_dict[key])
+            while (key == 'date' and value is digit):
+                value = input(tournament_dict[key])
+                print('cle, valeur\n', key, value, "\n")
+            while (key == 'turns_number' and value > 4):
+                value = input(tournament_dict[key])
+                print('cle, valeur\n', key, value, "\n")
+            while (key == 'time_controller' and (value > 0 and value < 4)):
+                value = input(tournament_dict[key])
+                print('cle, valeur\n', key, value, "\n")
             
-            
-        
         # valider ce qui rentre (ne pas accepter qu'il ne rentre rien du tout et n'accepter que les 3 types prévus, et nombres de tours, dates, etc. avec un retry)
         # fonction d'input avec validation
         
-        return tournament_dict
+        return tournament_info
 
 
     def prompt_add_player(self):
@@ -42,6 +50,3 @@ class TournamentView:
         # afficher liste tournois 1 ... n et demande de choisir  quel tourois, pareil pour players qui sont pas encore dans tournoi
         tournament_name = input('Please indicate the name of the tournament for which you want to add a player : ')
         player_name = input("Please indicate the player's name you want to add to the tournament : ")
-        
-
-    
