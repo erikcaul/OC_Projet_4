fc40b86b89d24448bb64966a9624abafd4a04f84
erikcaul <erikcaul@gmail.com>
2022-02-26 15:20:22 -0500

    continue match_making round1 done but other rounds not

--- controllers/tools.py
+++ controllers/tools.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import operator
 
 """tools module"""
 
@@ -12,7 +13,7 @@ class Tools:
         try:
             datetime.strptime(date_string, format)
             return True
-            
+
         except:
             print("This is the incorrect date string format. It should be DDMMYYYY")
             return False
@@ -38,7 +39,7 @@ class Tools:
         menu_instance = {}
         i = 1
         for element in list:
-                menu_instance[str(i)] = element.name 
+                menu_instance[str(i)] = element.name
                 print(str(i) + '. ' + element.name)
                 i += 1
         return menu_instance
@@ -46,11 +47,45 @@ class Tools:
     def validate_menu_choice(self, choice, menu, list):
         try:
             if choice in menu:
-                number_in_list = int(choice) - 1 
+                number_in_list = int(choice) - 1
                 pick_up_element = list[number_in_list]
                 return pick_up_element
         except:
-            print("invalid option")  
+            print("invalid option")
+
+    # def split_list(self, list):
+        # half = len(list)//2
+        # return list[:half], list[half:]
 
     def match_making(self, tournament):
-        pass
+        # Au début du premier tour, triez tous les joueurs en fonction de leur classement.
+        # trier tous les joueurs en fonction de leur classement
+        tournament_players = tournament.players
+        ranking_dict = {}
+        for player in tournament_players:
+            player_name = player.name
+            player_ranking = player.ranking
+            ranking_dict[player_name] = player_ranking
+        print(ranking_dict)
+        sorted_ranking_list = sorted(ranking_dict.items(), key=operator.itemgetter(1))
+        print(sorted_ranking_list)
+
+        # Divisez les joueurs en deux moitiés, une supérieure et une inférieure.
+        half = len(sorted_ranking_list)//2
+        list1 = sorted_ranking_list[:half]
+        list2 = sorted_ranking_list[half:]
+        print(list1)
+        print(list2)
+        # Le meilleur joueur de la moitié supérieure est jumelé avec le meilleur joueur de la moitié inférieure,
+        # et ainsi de suite.
+        # Si nous avons huit joueurs triés par rang, alors le joueur 1 est jumelé avec le joueur 5, le joueur 2 est jumelé avec le joueur 6, etc.
+        round1 = {}
+        i = 0
+        for element1 in list1:
+            round1[i+1] = (element1[0], list2[i][0])
+            i+=1
+        print(round1)
+        # Au prochain tour, triez tous les joueurs en fonction de leur nombre total de points.
+        # Si plusieurs joueurs ont le même nombre de points, triez-les en fonction de leur rang.
+        # Associez le joueur 1 avec le joueur 2, le joueur 3 avec le joueur 4, et ainsi de suite. Si le joueur 1 a déjà joué contre le joueur 2, associez-le plutôt au joueur 3.
+        # Répétez les étapes 3 et 4 jusqu'à ce que le tournoi soit terminé.
--- controllers/tournament_controller.py
+++ controllers/tournament_controller.py
@@ -10,35 +10,35 @@ class TournamentController:
     def __init__(self, players_all):
         self.tournament_view = TournamentView()
         self.tools = Tools()
-        self.players_all = players_all # list of all players 
+        self.players_all = players_all # list of all players
         self.rounds = [] # rounds list
         self.tournaments_list = [] # liste de tournament
-        
+
 
     def new_tournament(self):
         new_tournement_info = self.tournament_view.prompt_tournament_creation()
         rounds = []
         players = [] # liste des players du tournament
         new_tournament = Tournament(
-            new_tournement_info["name"], 
-            new_tournement_info["location"], 
-            new_tournement_info["date"], 
-            rounds, 
-            players, 
+            new_tournement_info["name"],
+            new_tournement_info["location"],
+            new_tournement_info["date"],
+            rounds,
+            players,
             new_tournement_info["time_controller"],
-            new_tournement_info["turns_number"], 
+            new_tournement_info["turns_number"],
             new_tournement_info["description"]
-            )       
+            )
         self.tournaments_list.append(new_tournament)
 
     def add_player(self):
         pick_tournament = self.tournament_view.pick_up_tournament(self.tournaments_list)
-        if pick_tournament is None: 
+        if pick_tournament is None:
             return
         if len(pick_tournament.rounds) != 0:
-            return 
+            return
         pick_player = self.tournament_view.pick_up_player(self.players_all, pick_tournament.players)
-        pick_tournament.players.append(pick_player) 
+        pick_tournament.players.append(pick_player)
 
     def create_a_round(self):
         pick_tournament = self.tournament_view.pick_up_tournament(self.tournaments_list)
@@ -47,6 +47,3 @@ class TournamentController:
             print("Add more player please ! ")
             return
         self.tools.match_making(pick_tournament)
-        print(pick_tournament.name)
-        print(pick_tournament.players)
-        print(tournament_players_count)
